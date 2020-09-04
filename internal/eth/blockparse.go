cdae356048239be8d98eadeeab5560ef345bbc09
Rust Wizard <rw@rustwizard.net>
2020-09-04 18:37:04 +0300

    initial impl of fetch blocks method for eth client

--- internal/eth/blockparse.go
+++ internal/eth/blockparse.go
@@ -1,14 +1,41 @@
 package eth
 
 import (
+	"context"
+	"math/big"
+	"time"
+
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/pkg/errors"
 )
 
-func (c *Client) FetchBlocks() <-chan *types.Block {
+func (c *Client) FetchBlocks(fromblock int64) <-chan *types.Block {
 	out := make(chan *types.Block)
+
+	blockNum := big.NewInt(fromblock)
 	go func() {
 		defer close(out)
-		// TODO: impl logic
+		for {
+			time.Sleep(5 * time.Second)
+			block, err := c.blockByNumber(blockNum)
+			if err != nil {
+				c.errCh <- err
+				continue
+			}
+			out <- block
+		}
 	}()
 	return out
+}
+
+func (c *Client) blockByNumber(blocknum *big.Int) (*types.Block, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), c.conf.RequestTTL)
+	defer cancel()
+
+	block, err := c.cl.BlockByNumber(ctx, blocknum)
+	if err != nil {
+		return block, errors.Wrap(err, "fetch block: get block by number")
+	}
+
+	return block, nil
 }
